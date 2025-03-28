6ac1e358cc0e4449124ab8415fe32f52802b2e8c
mossimka <maksimsarsekeev@gmail.com>
2025-03-28 16:47:28 +0500

    move

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ShopItemsComponent } from "./shop-items/shop-items.component";
+import { Product } from './product';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+   standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'music-shop';
+
+  logged_in:boolean = false;
+
+  sign_up() {
+
+  }
+
+  login() {
+
+  }
+}
